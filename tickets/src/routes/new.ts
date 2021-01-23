04d332b70ec230ed312403b83419272c82d532c1
ghsamm <ghsamm@gmail.com>
2021-01-23 22:10:58 +0100

    updates

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
-import { requrieAuth } from "@ghsamm-org/common/build/middlewares/require-auth";
+import { requireAuth } from "@ghsamm-org/common/build/middlewares/require-auth";
 
 const router = express.Router();
 
-router.post("/api/tickets", requrieAuth, (req: Request, res: Response) => {
+router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
